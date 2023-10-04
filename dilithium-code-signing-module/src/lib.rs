d199c07895ad8a2cfd136d6f08a6cc58d7d8c400
Kumar Mikkilineni Pavan (MS/ECL2) <wmu2kor@KOR-V-002FX.kor.apac.bosch.com>
2023-10-04 09:03:40 +0530

    Added Code Comments

--- dilithium-code-signing-module/src/lib.rs
+++ dilithium-code-signing-module/src/lib.rs
@@ -19,15 +19,24 @@ pub fn alloc(len: usize) -> *mut u8 {
 
 #[no_mangle]
 pub unsafe fn sign_data(data: *mut u8, data_len: usize) -> (*mut u8, *mut u8) {
+    //Reading data from linear memory
     let data = Vec::from_raw_parts(data, data_len, data_len);
+    //Generating Key Pair
     let keys = Keypair::generate();
+    //Extracting pk
     let mut pk = keys.public.to_vec();
+    //Signing the data
     let mut signature = keys.sign(&data).to_vec();
-    println!("{:?}", signature);
+    //Converting signature and pk to raw pointers
     let sig_ptr = signature.as_mut_ptr();
     let pk_ptr = pk.as_mut_ptr();
+    // take ownership of the memory block and
+    // ensure that its destructor is not
+    // called when the object goes out of scope
+    // at the end of the function
     std::mem::forget(signature);
     std::mem::forget(pk);
+    //Returning the pointers
     (sig_ptr, pk_ptr)
 }
 
@@ -40,16 +49,20 @@ pub unsafe fn verify_data(
     pk: *mut u8,
     pk_len: usize,
 ) -> i32 {
+    //Reading data, signature and pk from linear memory
     let data = Vec::from_raw_parts(data, data_len, data_len);
     let signature = Vec::from_raw_parts(signature, signature_len, signature_len);
     let pk = Vec::from_raw_parts(pk, pk_len, pk_len);
 
+    //Verifying the signature
     match verify(&signature, &data, &pk) {
         Ok(_) => return 1,
         Err(_) => return 0,
     }
 }
 
+//deallocator function for a memory block by calling std::mem::drop using the desired value,
+//which effectively takes ownership of data and goes out of scope.
 #[no_mangle]
 pub unsafe fn dealloc(ptr: *mut u8, size: usize) {
     let data = Vec::from_raw_parts(ptr, size, size);
--- wasm-api/src/main.rs
+++ wasm-api/src/main.rs
@@ -5,19 +5,17 @@ use rocket::serde::{json::Json, Deserialize, Serialize};
 use wasmtime::*;
 use wasmtime_wasi::WasiCtxBuilder;
 
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct SignatureResponse {
-    signature: String,
-    public_key: String,
-}
-
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct VerifyRequest<'a> {
     data: &'a str,
     signature: &'a str,
     public_key: &'a str,
+}
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct VerifyResponse {
+    is_verified: bool,
 }
 
 #[derive(Deserialize)]
@@ -26,33 +24,49 @@ struct SignRequest<'a> {
     data: &'a str,
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct SignResponse {
+    signature: String,
+    public_key: String,
+}
+
 #[post("/sign", data = "<sign_request>")]
-fn sign(sign_request: Json<SignRequest<'_>>) -> Json<SignatureResponse> {
-    // Define the WASI functions globally on the `Config`.
+fn sign(sign_request: Json<SignRequest<'_>>) -> Json<SignResponse> {
+    // Engines store global configuration preferences such as compilation settings, enabled features, etc.
     let engine = Engine::default();
 
+    //A Module is a compiled in-memory representation of an input WebAssembly binary.
     let module =
         Module::from_file(&engine, "dilithium_code_sign_module.wasm.multivalue.wasm").unwrap();
 
+    //Structure used to link wasm modules/instances together.
     let mut linker = Linker::new(&engine);
 
+    //WasiContext
     let wasi = WasiCtxBuilder::new()
         .inherit_stdio()
         .inherit_args()
         .unwrap()
         .build();
 
+    //A Store is a collection of WebAssembly instances and host-defined state.
+    //All WebAssembly instances and items will be attached to and refer to a Store.
+    //For example instances, functions, globals, and tables are all attached to a Store.
     let mut store = Store::new(&engine, wasi);
 
     wasmtime_wasi::add_to_linker(&mut linker, |s| s).unwrap();
 
+    //An instantiated WebAssembly module.
     let instance = linker.instantiate(&mut store, &module).unwrap();
 
+    //Exported memory
     let memory = instance
         .get_memory(&mut store, "memory")
         .ok_or(anyhow::format_err!("failed to find `memory` export"))
         .unwrap();
 
+    //Exported Functions
     let alloc_fn = instance
         .get_typed_func::<i32, i32>(&mut store, "alloc")
         .unwrap();
@@ -63,10 +77,12 @@ fn sign(sign_request: Json<SignRequest<'_>>) -> Json<SignatureResponse> {
         .get_typed_func::<(i32, i32), (i32, i32)>(&mut store, "sign_data")
         .unwrap();
 
+    //Allocating memory for data to be signed
     let data_ptr = alloc_fn
         .call(&mut store, sign_request.data.len() as i32)
         .unwrap();
 
+    //Copying data to be signed into allocated memory
     unsafe {
         let raw = memory.data_ptr(&mut store).offset(data_ptr as isize);
         raw.copy_from(
@@ -74,7 +90,7 @@ fn sign(sign_request: Json<SignRequest<'_>>) -> Json<SignatureResponse> {
             sign_request.data.len(),
         );
     };
-
+    //Calling the sign_data Function returns signature and public key pointers
     let (sig_ptr, pk_ptr) = sign_data_fn
         .call(
             &mut store,
@@ -82,64 +98,76 @@ fn sign(sign_request: Json<SignRequest<'_>>) -> Json<SignatureResponse> {
         )
         .unwrap();
 
+    //Reading signature from memory
     let mut sig_data = [0u8; 3293];
     memory
         .read(&store, sig_ptr as usize, &mut sig_data)
         .unwrap();
 
+    //Reading public key from memory
     let mut pk_data = [0u8; 1952];
     memory.read(&store, pk_ptr as usize, &mut pk_data).unwrap();
 
+    //Calling dealloc function to deallocate memory allocated for data to be signature
     dealloc_fn
         .call(&mut store, (sig_ptr, sig_data.len() as i32))
         .unwrap();
     dealloc_fn
         .call(&mut store, (pk_ptr, pk_data.len() as i32))
         .unwrap();
+    //Encoding signature and public key to Base64
     let sig_data_encoded = general_purpose::STANDARD.encode(&sig_data);
     let pk_data_encoded = general_purpose::STANDARD.encode(&pk_data);
-    Json(SignatureResponse {
+
+    //Returning signature and public key as JSON response
+    Json(SignResponse {
         signature: sig_data_encoded,
         public_key: pk_data_encoded,
     })
 }
 
 #[post("/verify", data = "<verify_request>")]
-fn verify(verify_request: Json<VerifyRequest<'_>>) -> Json<bool> {
-    // Define the WASI functions globally on the `Config`.
+fn verify(verify_request: Json<VerifyRequest<'_>>) -> Json<VerifyResponse> {
+    // Engines store global configuration preferences such as compilation settings, enabled features, etc.
     let engine = Engine::default();
 
+    //A Module is a compiled in-memory representation of an input WebAssembly binary.
     let module =
         Module::from_file(&engine, "dilithium_code_sign_module.wasm.multivalue.wasm").unwrap();
 
+    //Structure used to link wasm modules/instances together.
     let mut linker = Linker::new(&engine);
 
+    //WasiContext
     let wasi = WasiCtxBuilder::new()
         .inherit_stdio()
         .inherit_args()
         .unwrap()
         .build();
 
+    //A Store is a collection of WebAssembly instances and host-defined state.
+    //All WebAssembly instances and items will be attached to and refer to a Store.
+    //For example instances, functions, globals, and tables are all attached to a Store.
     let mut store = Store::new(&engine, wasi);
 
     wasmtime_wasi::add_to_linker(&mut linker, |s| s).unwrap();
 
+    //An instantiated WebAssembly module.
     let instance = linker.instantiate(&mut store, &module).unwrap();
-
+    //Exported memory
     let memory = instance
         .get_memory(&mut store, "memory")
         .ok_or(anyhow::format_err!("failed to find `memory` export"))
         .unwrap();
-
+    //Exported fucntions
     let alloc_fn = instance
         .get_typed_func::<i32, i32>(&mut store, "alloc")
         .unwrap();
-
     let verify_data_fn = instance
         .get_typed_func::<(i32, i32, i32, i32, i32, i32), i32>(&mut store, "verify_data")
         .unwrap();
 
-    //decode signature, public key
+    //decode signature, public key from Base64
     let signature_decoded = general_purpose::STANDARD
         .decode(&verify_request.signature)
         .unwrap();
@@ -148,7 +176,7 @@ fn verify(verify_request: Json<VerifyRequest<'_>>) -> Json<bool> {
         .unwrap();
     let data_decoded = verify_request.data.as_bytes().to_vec();
 
-    //copy signature, public key, data to wasm memory
+    //allocate memory for signature, public key, data
     let signature_ptr = alloc_fn
         .call(&mut store, signature_decoded.len() as i32)
         .unwrap();
@@ -159,6 +187,7 @@ fn verify(verify_request: Json<VerifyRequest<'_>>) -> Json<bool> {
         .call(&mut store, data_decoded.len() as i32)
         .unwrap();
 
+    //copy signature, public key, data to wasm memory
     unsafe {
         let raw = memory.data_ptr(&mut store).offset(signature_ptr as isize);
         raw.copy_from(signature_decoded.as_ptr(), signature_decoded.len());
@@ -174,6 +203,7 @@ fn verify(verify_request: Json<VerifyRequest<'_>>) -> Json<bool> {
         raw.copy_from(data_decoded.as_ptr(), data_decoded.len());
     };
 
+    //Calling verify_data function returns 0 if signature is valid and 1 if not
     let response = verify_data_fn
         .call(
             &mut store,
@@ -189,9 +219,9 @@ fn verify(verify_request: Json<VerifyRequest<'_>>) -> Json<bool> {
         .unwrap();
 
     if response != 0 {
-        Json(true)
+        Json(VerifyResponse { is_verified: true })
     } else {
-        Json(false)
+        Json(VerifyResponse { is_verified: false })
     }
 }
 
